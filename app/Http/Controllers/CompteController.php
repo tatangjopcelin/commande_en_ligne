f2b404f08e59bfa1f547c9294f3de59d8e4b5128
jopcelin <tatangjopcelin@gmail.com>
2024-05-15 14:11:02 +0200

    first commit

---
+++ app/Http/Controllers/CompteController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Compte;
+use App\Http\Requests\StoreCompteRequest;
+use App\Http\Requests\UpdateCompteRequest;
+
+class CompteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCompteRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Compte $compte)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Compte $compte)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCompteRequest $request, Compte $compte)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Compte $compte)
+    {
+        //
+    }
+}
